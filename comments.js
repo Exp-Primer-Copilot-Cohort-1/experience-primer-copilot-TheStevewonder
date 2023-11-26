8b56dd58d9a803dd70042ad2635092ca247fb80c
TheStevewonder <149306582+TheStevewonder@users.noreply.github.com>
2023-11-26 15:56:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,70 @@
-// create web server
-const http = require('http');
+//create web server 
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var comments = require('./comments');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+//create server
+var server = http.createServer(function(req,res){
+	//get url
+	var url = req.url;
+	//get method
+	var method = req.method.toLowerCase();
+	//get pathname
+	var pathname = url.split('?')[0];
+	//get query
+	var query = url.split('?')[1];
+	//get queryObj
+	var queryObj = {};
+	query && query.split('&').forEach(function(item){
+		var arr = item.split('=');
+		queryObj[arr[0]] = arr[1];
+	});
+	//get post data
+	var str = '';
+	req.on('data',function(data){
+		str += data;
+	});
+	req.on('end',function(){
+		var post = {};
+		str && str.split('&').forEach(function(item){
+			var arr = item.split('=');
+			post[arr[0]] = arr[1];
+		});
+		//pathname
+		if(pathname == '/'){
+			//read index.html
+			fs.readFile('./index.html',function(err,data){
+				if(err){
+					res.setHeader('Content-Type','text/plain;charset=utf-8');
+					res.end('文件读取失败，请稍后重试！');
+				}else{
+					res.setHeader('Content-Type','text/html;charset=utf-8');
+					res.end(data);
+				}
+			});
+		}else if(pathname == '/getComments'){
+			//get comments
+			comments.getComments(function(err,data){
+				if(err){
+					res.setHeader('Content-Type','text/plain;charset=utf-8');
+					res.end('文件读取失败，请稍后重试！');
+				}else{
+					res.setHeader('Content-Type','application/json;charset=utf-8');
+					res.end(JSON.stringify(data));
+				}
+			});
+		}else if(pathname == '/addComment'){
+			//add comment
+			comments.addComment(post,function(err,data){
+				if(err){
+					res.setHeader('Content-Type','text/plain;charset=utf-8');
+					res.end('文件读取失败，请稍后重试！');
+				}else{
+					res.setHeader('Content-Type','application/json;charset=utf-8');
+					res.end(JSON.stringify(data));
+				}
+			});
+		}else if(pathname == '/deleteComment'){
+			//delete comment
